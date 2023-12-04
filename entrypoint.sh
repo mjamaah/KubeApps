a030aab1b983b9e74b94689416c9c119b048d39b
mjamaah <mjamaah@madfoat.com>
2023-12-04 20:03:50 +0300

    Publish

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "$INPUT_KUBE_CONFIG" | base64 --decode > /tmp/config
+export KUBECONFIG=/tmp/config
+
+sh -c "$INPUT_ACTION $*"
